f6129300c7727a39b47cd16f3df44d3c1f12a373
William. W.B. <36773237+Acestus@users.noreply.github.com>
2024-11-15 02:09:45 +0000

    e

--- dsc02.ps1
+++ dsc02.ps1
@@ -1,6 +1,6 @@
 # Check if SecurityPolicyDsc module is installed
 $module = Get-Module -ListAvailable -Name SecurityPolicyDsc
-if ($null -eq $module) {
+if ($null -eq $module) 
     Install-Module -Name SecurityPolicyDsc -Force
 
 #Requires -Module SecurityPolicyDsc
