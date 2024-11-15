b5491a77a99113895f330ba4b5016eaffd71b9b3
William. W.B. <36773237+Acestus@users.noreply.github.com>
2024-11-15 02:01:07 +0000

    e

---
+++ asr-rules.ps1
@@ -0,0 +1,13 @@
+
+$rules = @(
+    "D4F940AB-401B-4EfC-AADC-AD5F3C50688A",  # Block executable content from email and webmail clients
+    "01443614-CD74-433A-B99E-2ECDC07BFC25",  # Block executable files from running unless they meet a prevalence, age, or trusted list criterion
+    "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2"   # Block credential stealing from LSASS
+)
+
+foreach ($rule in $rules) {
+    Add-MpPreference -AttackSurfaceReductionRules_Ids $rule -AttackSurfaceReductionRules_Actions Enabled
+}
+
+
+Get-MpPreference | Select-Object -ExpandProperty AttackSurfaceReductionRules_Actions
